25e2ce9ac7105e31ed2b732d2cd4ec745f159989
Raja HP Laptop <armorasha@yahoo.com>
2020-03-22 20:19:59 +1030

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Php-coding-practice" 
---
+++ php_test.php
@@ -0,0 +1,15 @@
+<?php
+
+function multiply($a, $b) {
+  return $a * $b;
+}
+
+echo multiply(3, 4) . "\n <br>";
+
+
+$age = array("Peter" => 35, "Ben" => 37, "Joe" => 43);
+
+echo json_encode($age);
+
+
+?>
