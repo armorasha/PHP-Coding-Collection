bd145e475921cda10eb7922b832eb8b6a0c67bd6
Raja WSL2 <armorasha@gmail.com>
2020-08-13 06:51:43 +0930

    git username ch

--- public_html/index.php
+++ public_html/index.php
@@ -3,7 +3,7 @@
 echo "<h2>See: C:\Users\\000930322\Downloads\PHP\PHP Summary.docx </h2><br><br>";
 
 echo "<h1>First LAMP project on WSL2. Moved out of XAMPP!</h1>";
-echo "No Docker - Just plain LAMP apache site as hosted in a AWS EC2 VM";
-echo "First LAMP project on WSL2. Moved out of XAMPP. Without Docker - Just plain LAMP as in a AWS EC2 VM apache site";
+echo "No Docker - Just plain LAMP stack apache site running in Ubuntu, similar to plain LAMP site running in XAMPP.";
+echo "Site is running as if it is hosted in a AWS EC2 Linux VM";
 
 ?>
