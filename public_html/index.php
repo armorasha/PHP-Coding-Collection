75c5885fe66c500081c3bfe1d3662d5cd39a5829
armorasha <github@yahoo.com>
2020-08-13 06:37:58 +0930

    Apache-PHP opcache delay fixed in just 3hrs

---
+++ public_html/index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "<h2>See: C:\Users\\000930322\Downloads\PHP\PHP Summary.docx </h2><br><br>";
+
+echo "<h1>First LAMP project on WSL2. Moved out of XAMPP!</h1>";
+echo "No Docker - Just plain LAMP apache site as hosted in a AWS EC2 VM";
+echo "First LAMP project on WSL2. Moved out of XAMPP. Without Docker - Just plain LAMP as in a AWS EC2 VM apache site";
+
+?>
