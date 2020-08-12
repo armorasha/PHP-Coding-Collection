a61275cf255a7c0571b774ad3a2dab059f8e4dd9
armorasha <github@yahoo.com>
2020-08-13 06:44:13 +0930

    git username ch

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "<h2>See: C:\Users\\000930322\Downloads\PHP\PHP Summary.docx </h2><br><br>";
+echo "<h2>Seee: C:\Users\\000930322\Downloads\PHP\PHP Summary.docx </h2><br><br>";
 
 echo "<h1>First LAMP project on WSL2. Moved out of XAMPP!</h1>";
 echo "No Docker - Just plain LAMP apache site as hosted in a AWS EC2 VM";
