65495e97a833130f29ae06269bd7a6eb26409fc6
Raja WSL2 <armorasha@yahoo.com>
2020-08-13 22:59:59 +0930

    index.php updated with links

--- public_html/index.php
+++ public_html/index.php
@@ -1,9 +1,17 @@
 <?php
-
-echo "<h2>For Steps to create simple exercise like this, CLI commands, troubleshooting- See: C:\Users\\000930322\Downloads\PHP\PHP Summary.docx </h2><br><br>";
-
 echo "<h1>First LAMP exercise on WSL2. Moved out of XAMPP!</h1>";
-echo "<p><b>No Docker</b> - Just plain LAMP stack apache site running in Ubuntu, similar to plain LAMP site running in XAMPP.</p>";
-echo "Site is running as if it is hosted in a AWS EC2 Linux VM.";
+echo '<h3><a href="phpinfo.php">phpinfo()</a></h3>';
+echo "<h3><b>No Docker.</b> <br> Just plain LAMP stack apache site running in Ubuntu,". 
+" similar to plain LAMP site running in XAMPP.Site is running as if it is ".
+"hosted in a AWS EC2 Linux VM.</h3><br>";
+
+echo '<h3>For Steps to create simple exercise like this, CLI commands, troubleshooting - See: </h3>';
+echo '<h3 style="color:red"> C:\Users\\000930322\Downloads\PHP\PHP Summary.docx </h3><br>';
 
 ?>
+
+
+<h2><u><b>Exercises</b></u></h2>
+
+<a href="0_misc_exercises">0_misc_exercises</a><br>
+<a href="1_class_object_exercise">1_class_object_exercise</a><br>
