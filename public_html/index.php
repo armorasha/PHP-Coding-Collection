a96e45c87f5a89c0475b0187c20569950d94b018
Raja Win10 PowerShell <armorasha@yahoo.com>
2020-08-13 18:05:57 +0930

    Windows powershell push test

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "<h2>See for more info: C:\Users\\000930322\Downloads\PHP\PHP Summary.docx </h2><br><br>";
+echo "<h2>See this doc: C:\Users\\000930322\Downloads\PHP\PHP Summary.docx </h2><br><br>";
 
 echo "<h1>First LAMP project on WSL2. Moved out of XAMPP!</h1>";
 echo "No Docker - Just plain LAMP stack apache site running in Ubuntu, similar to plain LAMP site running in XAMPP.";
