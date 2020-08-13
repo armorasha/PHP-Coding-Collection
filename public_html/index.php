02441bb0a73f9648226f75980f2d9146bb0709a3
Raja WSL2 <armorasha@yahoo.com>
2020-08-13 20:13:35 +0930

    Moved apache log files to default location

--- public_html/index.php
+++ public_html/index.php
@@ -1,9 +1,9 @@
 <?php
 
-echo "<h2>See this doc: C:\Users\\000930322\Downloads\PHP\PHP Summary.docx </h2><br><br>";
+echo "<h2>For Steps to create simple exercise like this, CLI commands, troubleshooting- See: C:\Users\\000930322\Downloads\PHP\PHP Summary.docx </h2><br><br>";
 
-echo "<h1>First LAMP project on WSL2. Moved out of XAMPP!</h1>";
-echo "No Docker - Just plain LAMP stack apache site running in Ubuntu, similar to plain LAMP site running in XAMPP.";
-echo "Site is running as if it is hosted in a AWS EC2 Linux VM";
+echo "<h1>First LAMP exercise on WSL2. Moved out of XAMPP!</h1>";
+echo "<p><b>No Docker</b> - Just plain LAMP stack apache site running in Ubuntu, similar to plain LAMP site running in XAMPP.</p>";
+echo "Site is running as if it is hosted in a AWS EC2 Linux VM.";
 
 ?>
