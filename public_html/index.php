e40c885cda04e8bf3e8085fee4a697ecbd187218
Raja WSL2 <armorasha@yahoo.com>
2020-08-14 23:16:34 +0930

    mdbootstrap complete

--- public_html/index.php
+++ public_html/index.php
@@ -34,36 +34,60 @@
 </head>
 
 <body>
-    <div class="box blue-gradient" style="--r1:130%;--r2:71.5%">
-        <div class="container-fluid text-center pt-5">
-            <?php
-            // echo '<h1 class="display-3 text-black-50">LAMP <i class="fas fa-server grey-text"></i>   Ubuntu <i class="fab fa-ubuntu red-text"></i></h1><br>';
-            echo '<h1 class="display-3 text-white ">LAMP <i class="fas fa-server grey-text"></i>   Ubuntu <i class="fab fa-ubuntu red-text"></i></h1><br>';
-            ?>
+
+    <div class="backy">
+        <div class="box-wrap sticky-top" style="z-index:9998;">
+            <div class="box blue-gradient" style="--r1:130%;--r2:71.5%;">
+                <div class="container-fluid text-center pt-5 ">
+                    <?php
+                    echo '<h1 class="display-3 text-white ">LAMP <i class="fas fa-server grey-text"></i>   Ubuntu <i class="fab fa-ubuntu red-text"></i></h1><br>';
+                    ?>
+                </div>
+            </div>
         </div>
+
+
+        <div class="jumbotron card card-image" style="background-image: url('img/gradient1.jpg');">
+            <div class="text-white text-center px-4">
+                <div class="row">
+                    <div class="column ml-5">
+                        <h1 class="card-title display-3 pt-3 mb-5 font-bold"><strong>Windows Subsystem for Linux 2</strong></h1>
+                        <h2 class="card-title h1-responsive pt-3 mb-5 font-bold"><strong>No XAMPP <i class="fab fa-windows teal-text"></i>&emsp; No Docker <i class="fab fa-docker blue-text"></i></strong></h2>
+
+                    </div>
+                    <div class="column mx-auto">
+                        <h1 class="card-title display-3 pt-3 mb-5 font-bold"><strong><i class="fab fa-linux red-text fa-3x"></i></strong></h1>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <a class="btn btn-outline-white btn-lg  mx-auto mt-5"><i class="fas fa-clone left"></i> View project</a>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="container cloudy-knoxville-gradient text-black-50 z-depth-1-half my-5 p-5">
+
+            <h3><i class="fas fa-angle-right"></i></h3>
+            <h3 class="text-monospace"><a href="phpinfo.php">phpinfo();</a></h3>
+            <h6>Just plain LAMP stack apache site running in Ubuntu,
+                similar to plain LAMP site running in XAMPP. Site is running as if it is
+                hosted in a AWS EC2 Linux VM.</h6><br><br>
+
+            <h3><i class="fas fa-angle-right"></i></h3>
+            <h4 style="color:red"><b>C:\Users\\000930322\Downloads\PHP\PHP Summary.docx</b></h4>
+            <h6>Has info on steps to create a simple exercise, CLI commands, troubleshooting</h6><br><br>
+
+        </div>
+        <div class="spacer invisible my-2">
+            <h1>jkhfsd</h1>
+        </div>
+
     </div>
 
-    <div class="container cloudy-knoxville-gradient text-black-50 z-depth-1-half my-5 p-5">
 
-        <h3><i class="fas fa-angle-right"></i></h3>
-        <h4><b><i class="far fa-times-circle red-text"></i> XAMPP <i class="fab fa-windows indigo-text"></i></b></h3><br>
-        <h4><b><i class="far fa-times-circle red-text"></i> Docker <i class="fab fa-docker cyan-text"></i></b></h3><br>
-        <h4><b><i class="far fa-check-circle green-text"></i> WSL2 <i class="fab fa-linux amber-text"></i></b></h3><br><br><br>
-
-
-        <h3><i class="fas fa-angle-right"></i></h3>
-        <h3 class="text-monospace"><a href="phpinfo.php">phpinfo();</a></h3>
-        <h6>Just plain LAMP stack apache site running in Ubuntu,
-            similar to plain LAMP site running in XAMPP. Site is running as if it is
-            hosted in a AWS EC2 Linux VM.</h6><br><br>
-
-        <h3><i class="fas fa-angle-right"></i></h3>
-        <h4 style="color:red"><b>C:\Users\\000930322\Downloads\PHP\PHP Summary.docx</b></h4>
-        <h6>Has info on steps to create a simple exercise, CLI commands, troubleshooting</h6><br><br>
-
-    </div>
-
-    <div class="container z-depth-5 my-5 p-5">
+    <div class="container cloudy-knoxville-gradient z-depth-5 my-5 p-5">
         <h2><b><i class="fas fa-file-code"></i> Exercises</b></h2>
 
         <a href="0_misc_exercises">0_misc_exercises</a><br>
@@ -77,6 +101,8 @@
         <a href="2_composer_phpunit">2_composer_phpunit - Classes</a>
         <p><b>2_composer_phpunit - Tests</b> in <b>tests folder</b> outside public_html. Not reachable in localhost. More info on this exercise at:</p>
         <p style="color:red"> C:\Users\\000930322\Downloads\PHP\PHP Summary.docx </p><br>
+
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
     </div>
 
